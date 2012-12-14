7599a9f7ab6b08e7d35bc28d0d68b51c5c811ca2
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-12-14 06:54:33 +0000

    Version 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "roy@pioneeringsoftware.co.uk"
 license          "All rights reserved"
 description      "Installs/Configures resque_work"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 depends "god"
